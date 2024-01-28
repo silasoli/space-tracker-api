4f4ad13142cb11e08625ee398efae066d055e32d
silasoli <silasoliv39@gmail.com>
2024-01-28 17:08:53 -0300

    fix: adjust erros in test

--- src/auth/services/auth.service.spec.ts
+++ src/auth/services/auth.service.spec.ts
@@ -1,18 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UsersService } from '../../users/services/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let usersService: UsersService;
+  let jwtService: JwtService;
+
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: JwtService,
+          useValue: {}
+        },
+        {
+          provide: UsersService,
+          useValue: {}
+        }
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
+    usersService = module.get<UsersService>(UsersService);
+    jwtService = module.get<JwtService>(JwtService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+    expect(usersService).toBeDefined();
+    expect(jwtService).toBeDefined();
   });
 });
--- src/rentals/services/rentals.service.spec.ts
+++ src/rentals/services/rentals.service.spec.ts
@@ -1,18 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RentalsService } from './rentals.service';
+import { Rental, RentalDocument } from '../schemas/rental.entity';
+import { getModelToken } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 describe('RentalsService', () => {
   let service: RentalsService;
+  let rentalModel: Model<RentalDocument>;
+
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RentalsService],
+      providers: [
+        RentalsService,
+        {
+          provide: getModelToken(Rental.name),
+          useValue: {}
+        }
+      ],
     }).compile();
 
     service = module.get<RentalsService>(RentalsService);
+    rentalModel = module.get<Model<RentalDocument>>(getModelToken(Rental.name));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+    expect(rentalModel).toBeDefined();
   });
 });
